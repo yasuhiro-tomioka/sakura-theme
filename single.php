81b62352b416796ed632e864a06fe3c44247a086
富岡康博 <ytomioka@yasuhirotno-MacBook-Pro.local>
2020-11-30 19:24:08 +0900

    1130 1st commit

--- single.php
+++ single.php
@@ -1,7 +1,7 @@
-<?php wp_head(); ?>
+<?php get_header(); ?>
 <!-- the_postは書いておくべき。投稿ページを読み込むときに必要な関数 -->
 <?php the_post(); ?>
 <div><?php the_post_thumbnail('category-thumb'); ?></div>
 <h1><?php the_title(); ?></h1>
 <p><?php the_content(); ?></p>
-<?php wp_footer(); ?>
+<?php get_footer(); ?>
